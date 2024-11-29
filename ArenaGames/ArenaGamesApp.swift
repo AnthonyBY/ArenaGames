6cbb7897fff3cef6bcf071726d8e011bf9bc6e00
Anton Marchanka <anthonymarchenko@gmail.com>
2024-11-29 22:06:32 +0400

    Initial Commit

---
+++ ArenaGames/ArenaGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArenaGamesApp.swift
+//  ArenaGames
+//
+//  Created by Anton Marchanka on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ArenaGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
